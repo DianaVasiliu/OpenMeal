66431f5bd940827215f77d9170c0d6f3f40aef60
Mira Stroie <miramaria27@yahoo.com>
2022-01-12 21:20:53 +0200

    Add the cup back.

--- OpenMeal/Scene.cpp
+++ OpenMeal/Scene.cpp
@@ -142,12 +142,14 @@ void Scene::InitializeScene() {
 	Model* burger = new Model("burger1.obj");
 	Model* plate = new Model("plate1.obj");
 	Model* book = new Model("book3.obj");
-	Model* cup = new Model("glass2.obj");
+	Model* cup = new Model("cup2.obj");
+	Model* glass = new Model("glass2.obj");
 
 	models.push_back(table);
 	models.push_back(burger);
 	models.push_back(book);
 	models.push_back(cup);
+	models.push_back(glass);
 	models.push_back(plate);
 
 	// Creare VBO+shader
@@ -251,21 +253,37 @@ void Scene::RenderFunction() {
 	//draw the book
 	glDrawArrays(GL_TRIANGLES, 0, models[i]->verticesCount);
 
-	
 	// drawing the cup
+	i++;
+	glBindVertexArray(models[i]->VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, models[i]->VAO);
+	// set the cup's position
+	glm::mat4 resizeCup = glm::scale(glm::mat4(1.0f), glm::vec3(0.0035, 0.0035, 0.0035));
+	glm::mat4 translateCup = glm::translate(glm::mat4(1.0f), glm::vec3(-3.5, 6.2, 0));
+	myMatrix = translateCup * resizeCup;
+	glUniformMatrix4fv(myMatrixLocation, 1, GL_FALSE, &myMatrix[0][0]);
+
+	// new texture for the cup
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, CupTexture);
+	glUniform1i(glGetUniformLocation(ProgramId, "myTexture"), 0);
+	
+	//draw the cup
+	glDrawArrays(GL_TRIANGLES, 0, models[i]->verticesCount);
+
+	// drawing the glass
 	i ++;
 	glBindVertexArray(models[i]->VAO);
 	glBindBuffer(GL_ARRAY_BUFFER, models[i]->VAO);
 	glBindTexture(GL_TEXTURE_2D, 0);
-	// set the cup's position
-	glm::mat4 resizeCup = glm::scale(glm::mat4(1.0f), glm::vec3(0.25, 0.25, 0.25));
-	glm::mat4 translateCup = glm::translate(glm::mat4(1.0f), glm::vec3(-3.5,6.2, 0));
-	myMatrix = translateCup * resizeCup;
+	// set the glass's position
+	glm::mat4 resizeGlass = glm::scale(glm::mat4(1.0f), glm::vec3(0.25, 0.25, 0.25));
+	glm::mat4 translateGlass = glm::translate(glm::mat4(1.0f), glm::vec3(-4,6.2, -1.5));
+	myMatrix = translateGlass * resizeGlass;
 	glUniformMatrix4fv(myMatrixLocation, 1, GL_FALSE, &myMatrix[0][0]);
 
 
-	//draw the cup
-	
+	//draw the glass
 	glDrawArrays(GL_TRIANGLES, 0, models[i]->verticesCount);
 
 
